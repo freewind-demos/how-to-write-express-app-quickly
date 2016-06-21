d7914da43968064f23459cf1052b9ea2ac4c9949
Peng.Li <peng.li@rea-group.com>
2016-06-21 19:49:58 +0800

    say hello

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello');
+});
+
+var server = app.listen(3000,function () {
+  var port = server.address().port;
+  console.log("listen on " + port);
+});
